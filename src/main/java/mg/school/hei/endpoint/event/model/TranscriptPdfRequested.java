686eccc1ac2b8ba24e31229f3598ad9b0ac3482f
Rindra <hei.rindra.2@gmail.com>
2026-08-18 03:41:01 +0300

    feat: add TranscriptPdfRequested event model

---
+++ src/main/java/mg/school/hei/endpoint/event/model/TranscriptPdfRequested.java
@@ -0,0 +1,36 @@
+package mg.school.hei.endpoint.event.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.Duration;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import mg.school.hei.PojaGenerated;
+
+@PojaGenerated
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ToString
+public class TranscriptPdfRequested extends PojaEvent {
+  @JsonProperty("studentId")
+  private String studentId;
+
+  @JsonProperty("recipientEmail")
+  private String recipientEmail;
+
+  @Override
+  public Duration maxConsumerDuration() {
+    return Duration.ofSeconds(60);
+  }
+
+  @Override
+  public Duration maxConsumerBackoffBetweenRetries() {
+    return Duration.ofSeconds(30);
+  }
+}
